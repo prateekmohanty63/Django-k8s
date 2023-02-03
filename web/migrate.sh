3c28aaa79957b5c5d13360fe37fc71444f89042e
Prateek <prateekmohanty63@gmail.com>
2023-02-03 20:08:16 +0530

    Feat(POSTGRES) Postgres database setup done

--- web/migrate.sh
+++ web/migrate.sh
@@ -3,7 +3,7 @@
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"prateekmohanty63@gmail.com"}
 cd /app/
 
-/opt/env/bin/python manage.py migrate --noinput
-/opt/env/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+/opt/venv/bin/python manage.py migrate --noinput
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
 
 
